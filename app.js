8283a90c7ab0f787c27fa092da57c33c4c99a00a
Anne Ensign <anne.ensign@gmail.com>
2021-06-07 21:26:46 -0400

    Add python to run on local server to view JSX

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-const title = React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is a Great Title'},
-    'My First React Element'
-);
+/**
+ * React does not create real DOM nodes
+ * (elements like h1, div, span, etc)
+ * Instead, React creates plain JS objects that describe DOM nodes
+ */
+//-------------------------------------------------------------------------------------
 
-const description =  React.createElement(
-    'p',
-    null,
-    'I just learned how to create a React node and render it to the DOM!'
-);
+
+/**
+ * JSX: A syntax extension to JS that is used 
+ * with React to describe elements in the UI
+ * (rather than using React.createElement repeatedly)
+ * Babel script tag for browser to read it
+ */
+const title = <h1>'My First React Element'</h1>
+const description = <p>'I just learned how to create a React node and render it to the DOM!'</p>
 
 const header = React.createElement(
     'header',
@@ -21,3 +26,4 @@ ReactDOM.render(
     header,
     document.getElementById('root')
 );
+
