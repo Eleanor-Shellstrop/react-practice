ce7783d3bbb7db069d2ca951c6aa078d2c1e475e
Anne Ensign <anne.ensign@gmail.com>
2021-06-09 10:29:56 -0400

    Make function for Header component

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 // Starting new phase: Making a scoreboard app
 
+// React UI is a composition of functions
+// React components are required to start with an upper case letter
+function Header() {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    );
+}
+
+// Add self closing tag if no children present
 ReactDOM.render(
-    header,
+    <Header />,
     document.getElementById('root')
 );
 
