4a54be8d95ef6ff1191795e0e1a1a28a3bc5078b
Anne Ensign <anne.ensign@gmail.com>
2021-06-09 10:51:54 -0400

    Add Counter and App components

--- app.js
+++ app.js
@@ -1,16 +1,48 @@
-//  Often see components defined as arrow functions.
-//  If writing a simple function that just returns JSX,
-//  you can use an implicit return.
-//  Parenthesis optional
-const  Header = () => (
-    <header>
-        <h1>Scoreboard</h1>
-        <span className="stats">Players: 1</span>
-    </header>
-);
+const  Header = () => {
+    return (
+        <header>
+            <h1>Scoreboard</h1>
+            <span className="stats">Players: 1</span>
+        </header>
+    ); 
+}
 
+//  Nest components: 
+//  When a component countains another component, it's called composition
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">Anne</span>
+            <Counter />            
+        </div>
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">35</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
+}
+
+const App = () => {
+    return(
+        <div className="scoreboard">
+            <Header />
+
+            {/* Players list*/}
+            <Player />
+        </div>
+    );
+}
+
+//  Usually renders your top level element into the DOM,
+//  So change to "App"
 ReactDOM.render(
-    <Header />,
+    <App />,
     document.getElementById('root')
 );
 
