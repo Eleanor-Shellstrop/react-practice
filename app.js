f18472e7e8da3196c7eef8537979e4a245f2b566
Anne Ensign <anne.ensign@gmail.com>
2021-06-09 11:42:39 -0400

    Add props to display data

--- app.js
+++ app.js
@@ -1,40 +1,72 @@
-const  Header = () => {
+const players = [
+    {
+        name: "Guil",
+        score: 50
+      },
+      {
+        name: "Treasure",
+        score: 85
+      },
+      {
+        name: "Ashley",
+        score: 95
+      },
+      {
+        name: "James",
+        score: 80
+      }
+]
+
+const  Header = (props) => {
     return (
         <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <h1>{ props.title }</h1>
+            <span className="stats">Players: { props.totalPlayers }</span>
         </header>
     ); 
 }
 
 //  Nest components: 
 //  When a component countains another component, it's called composition
-const Player = () => {
+const Player = (props) => {
     return (
         <div className="player">
-            <span className="player-name">Anne</span>
-            <Counter />            
+            <span className="player-name">
+                { props.name }
+            </span>
+            <Counter score={ props.score } />            
         </div>
     );
 }
 
-const Counter = () => {
+const Counter = (props) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">35</span>
+            <span className="counter-score">
+                { props.score }
+            </span>
             <button className="counter-action increment"> + </button>
         </div>
     );
 }
 
-const App = () => {
+const App = (props) => {
     return(
         <div className="scoreboard">
-            <Header />
+            <Header 
+                title="Scoreboard" 
+                totalPlayers={ props.initialPlayers.length } />
 
             {/* Players list*/}
-            <Player />
+            {props.initialPlayers.map( player =>
+            // Implicit return here
+                <Player 
+                    name={ player.name }
+                    score={ player.score } 
+                />
+            )}
+            
         </div>
     );
 }
@@ -42,7 +74,7 @@ const App = () => {
 //  Usually renders your top level element into the DOM,
 //  So change to "App"
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={ players }/>,
     document.getElementById('root')
 );
 
